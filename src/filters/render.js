f7684d4388070737e6b71862ebda56462491e5fc
Ken Hawkins <khawkins98@gmail.com>
2019-07-16 12:32:02 +0200

    Render tag: Add suport for escaping, cleanup, highlighting

--- src/filters/render.js
+++ src/filters/render.js
@@ -2,6 +2,10 @@
 // A port-fork of the Fractal render extension for Nunjucks for 11ty
 // Original: https://github.com/frctl/nunjucks/blob/develop/src/extensions/render.js
 
+// {% render '@'+variant.handle, variant.context, false, { escape: false, beautify: true, codetype: 'html', highlight: true } %}
+// - second property is for merge (currently not implemented but option captured)
+// - third option set escaping of code, beautify (formatting), code type (default: html) and highlighting (hljs)
+
 module.exports = function (nunjucksEngine,fractal) {
 
   this.tags = ["render"];
@@ -23,6 +27,11 @@ module.exports = function (nunjucksEngine,fractal) {
       const handle = args[0];
       let context = args[1];
       const merge = args[2] || false;
+      args[3] = args[3] || {};
+      const escape = args[3].escape || false;
+      const beautify = args[3].beautify || false;
+      const codetype = args[3].codetype || 'html';
+      const highlight = args[3].highlight || false;
       const entity = source.find(handle);
       if (!entity) {
           throw new Error(`Could not render component '${handle}' - component not found.`);
@@ -39,7 +48,21 @@ module.exports = function (nunjucksEngine,fractal) {
           // let env = JSON.parse(JSON.stringify(rootContext._env));
           // context._env = env;
           entity.render(context).then(html => {
-              let ret = new nunjucksEngine.runtime.SafeString(html);
+              let ret;
+              if (beautify) {
+                const beautify = require('js-beautify').html;
+                html = beautify(html,  { indent_size: 2, space_in_empty_paren: true });
+              }
+              if (highlight) {
+                const hljs = require('highlight.js');
+                html = hljs.highlight(codetype, html).value;
+              }
+              if (escape) {
+                // escaped by default, so just don't pass a safe string
+                ret = html;
+              } else {
+                ret = new nunjucksEngine.runtime.SafeString(html);
+              }
               callback(null, ret);
           }).catch(err => {
               callback(err);
