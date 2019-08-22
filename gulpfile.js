56c710f06aa44a8ba363213289b72ca55df6742f
Ken Hawkins <khawkins98@gmail.com>
2019-08-22 14:03:32 +0200

    Update to vf-core beta.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
 const gulp  = require('gulp');
-const rename = require('gulp-rename');
-
-let fractalBuildMode = 'build';
 
 // -----------------------------------------------------------------------------
 // Configuration
@@ -12,6 +9,11 @@ let fractalBuildMode = 'build';
 //   "vfName": "My Component Library",
 //   "vfNameSpace": "myco-",
 //   "vfComponentPath": "./src/components",
+//   "vfComponentDirectories": [
+//      "vf-core-components",
+//      "../node_modules/your-optional-collection-of-dependencies"
+//     NOTE: Don't forget to symlink: `cd components` `ln -s ../node_modules/your-optional-collection-of-dependencies`
+//    ],
 //   "vfBuildDestination": "./build",
 //   "vfThemePath": "@frctl/mandelbrot"
 // },
@@ -23,17 +25,37 @@ const config = JSON.parse(fs.readFileSync('./package.json'));
 config.vfConfig = config.vfConfig || [];
 global.vfName = config.vfConfig.vfName || "Visual Framework 2.0";
 global.vfNamespace = config.vfConfig.vfNamespace || "vf-";
-global.vfComponentPath = config.vfConfig.vfComponentPath || __dirname + '/src/components';
+global.vfComponentPath = config.vfConfig.vfComponentPath || path.resolve('.', __dirname + '/components');
 global.vfBuildDestination = config.vfConfig.vfBuildDestination || __dirname + '/temp/build-files';
 global.vfThemePath = config.vfConfig.vfThemePath || './tools/vf-frctl-theme';
-const componentPath = path.resolve('.', global.vfComponentPath);
-const buildDestionation = path.resolve('.', global.vfBuildDestination);
+global.vfVersion = config.version || 'not-specified';
+const componentPath = path.resolve('.', global.vfComponentPath).replace(/\\/g, '/');
+const componentDirectories = config.vfConfig.vfComponentDirectories || ['vf-core-components'];
+const buildDestionation = path.resolve('.', global.vfBuildDestination).replace(/\\/g, '/');
 
 // Some Gulp tasks live in their own files, for the sake of clarity.
 require('require-dir')('./gulp-tasks');
 
+// HACK: in vf-core@beta.2 a bug was introduced where it would erronously try to generate a version from the component template, this works around it by renaming the file
+// this can be removed once beta.3 is released
+
+// part 1: unset hack on init
+let hackComponentGeneratorPath = './node_modules/@visual-framework/vf-core/tools/component-generator/templates/';
+fs.rename(hackComponentGeneratorPath+'_ignored.json', hackComponentGeneratorPath+'_package.json', function (err) {
+  if (err) { /* file has already been moved */ }
+});
+
+// part 2. use hack when requested
+gulp.task('component-bug-hack', function(done) {
+  let hackComponentGeneratorPath = './node_modules/@visual-framework/vf-core/tools/component-generator/templates/';
+  fs.rename(hackComponentGeneratorPath+'_package.json', hackComponentGeneratorPath+'_ignored.json', function (err) {
+    if (err) { /* file has already been moved */ }
+  });
+  done();
+});
+
 // Tasks to build/run vf-core component system
-require('./node_modules/\@visual-framework/vf-core/tools/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, buildDestionation);
+require('./node_modules/\@visual-framework/vf-core/tools/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
 
 // Eleventy config
 process.argv.push('--config=eleventy.js');
@@ -79,17 +101,20 @@ gulp.task('eleventy', function(done) {
 });
 
 // Let's build this sucker.
+let fractalBuildMode = 'build';
 gulp.task('build', gulp.series(
+  'component-bug-hack',
   'vf-clean',
-  gulp.parallel('css','js','vf-css', 'vf-scripts','vf-component-assets'),
+  gulp.parallel('css','js','vf-css','vf-scripts','vf-component-assets'),
   'elventy-set-to-build',
   'eleventy'
 ));
 
 // Build and watch things during dev
 gulp.task('dev', gulp.series(
+  'component-bug-hack',
   'vf-clean',
-  gulp.parallel('css','js','vf-css', 'vf-scripts','vf-component-assets'),
+  gulp.parallel('css','js','vf-css','vf-scripts','vf-component-assets'),
   'elventy-set-to-serve',
   'eleventy',
   'watch'
