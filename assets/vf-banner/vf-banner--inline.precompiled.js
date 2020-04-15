7cbca73778e8de04f2319279990a5ff5f408eaaa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-15 20:04:24 +0000

    Deploy visual-framework/vf-eleventy to github.com/visual-framework/vf-eleventy.git:gh-pages

--- assets/vf-banner/vf-banner--inline.precompiled.js
+++ assets/vf-banner/vf-banner--inline.precompiled.js
@@ -8,7 +8,7 @@ var colno = 0;
 var output = "";
 try {
 var parentTemplate = null;
-output += "<div class=\"vf-banner vf-banner--phase\">\n  <div class=\"vf-banner__content\">\n    <p class=\"vf-text-body--3\">This is a new web page. <a href=\"";
+output += "<div class=\"vf-banner vf-banner--phase\">\n  <div class=\"vf-banner__content\">\n    <p class=\"vf-banner__text\">This is a new web page. <a href=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "vf") - runtime.contextOrFrameLookup(context, frame, "banner") - -runtime.contextOrFrameLookup(context, frame, "inline_href"), env.opts.autoescape);
 output += "\" class=\"vf-link\">Complete our quick survey</a> to help us make it better.</p>\n  </div>\n</div>\n";
 if(parentTemplate) {
